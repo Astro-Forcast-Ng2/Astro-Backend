d096a5b7eb7afcdb97d6129373556527e595b098
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-09-17 11:19:38 -1000

    Got Nasa sounds working as well

--- app/Http/Controllers/AstroObjsController.js
+++ app/Http/Controllers/AstroObjsController.js
@@ -23,6 +23,23 @@ class AstroObjsController {
                return res.json(body);        
             } );
     };
+
+    * sounds(req, res){
+        let options = {
+            method: 'GET',
+            uri: 'https://api.nasa.gov/planetary/sounds?api_key=' + nasaKey,
+            data:{
+                'q': 'apollo',
+            },
+            json: true // Automatically stringifies the body to JSON 
+        };
+        console.log(options.uri)
+        rp(options)
+            .then(function (body) {
+               return res.json(body);        
+            } );
+
+    }
 }
 
 module.exports = AstroObjsController
